09bb66f05b7ac7d54139737e1eb22a10f5aebdae
Shiqiang Xia <xiaxx268@umn.edu>
2021-03-08 10:57:07 -0600

    test local matrix k =1 correct

--- 03-Basis&Quad&Integral/Integrals/Volume_Int_u_du.m
+++ 03-Basis&Quad&Integral/Integrals/Volume_Int_u_du.m
@@ -13,14 +13,14 @@ function [Auur,Auus] = Volume_Int_u_du(k,GQ1DRef_pts,GQ1DRef_wts)
     b_list = reshape(m,[],1);
     
     mat_b = (1-b_list)/2;
-    mat_b = reshape(mat_b,[],Nu);
+    mat_b = reshape(mat_b,[],NGQ);
     
     
     % step 2 get Vandermonde and GradVandermonde matrices.
     
     V2D = Vandermonde2D(k,a_list,b_list);
     
-    [V2Dr,V2Ds] = GradVandermonde2D(N,a_list,b_list);
+    [V2Dr,V2Ds] = GradVandermonde2D(k,a_list,b_list);
     
     % step 3 do the quadrature.
     
@@ -28,16 +28,16 @@ function [Auur,Auus] = Volume_Int_u_du(k,GQ1DRef_pts,GQ1DRef_wts)
     for ii = 0:k
         for jj = 0:k-ii
             temp1 = V2D(:,ct);
-            temp1 = reshape(temp1,[],Nu);
+            temp1 = reshape(temp1,[],NGQ);
             sk = 1;
             
             for mm = 0:k
                 for nn = 0:k-mm
                     
                     temp2r = V2Dr(:,sk);
-                    temp2r = reshape(temp2r,[],Nu);
+                    temp2r = reshape(temp2r,[],NGQ);
                     temp2s = V2Ds(:,sk);
-                    temp2s = reshape(temp2s,[],Nu);
+                    temp2s = reshape(temp2s,[],NGQ);
                     
                     Auur(ct,sk) = GQ1DRef_wts'*(temp1.*temp2r.*mat_b)*GQ1DRef_wts;
                     Auus(ct,sk) = GQ1DRef_wts'*(temp1.*temp2s.*mat_b)*GQ1DRef_wts;
