a655037c2284fdb72f8922bd201ccd8b134e7e86
Shiqiang Xia <xiaxx268@umn.edu>
2021-03-26 13:18:21 -0500

    add maxwell framework

--- 01-ProblemDriver/main.m
+++ 01-ProblemDriver/main.m
@@ -1,11 +1,27 @@
-function main(varargin)
+function main(pde_type,varargin)
     
-    % example:  main('order',1, 'h0',0.2, 'Niter',3, 'refine_flag', 0);
-    
-    para = SetParameters(varargin{:});
+    % example:  
+    % main('elliptic','order',1, 'h0',0.2, 'Niter',3, 'refine_flag', 0);
     
     global class_t;
-    class_t=para.precision;
     
-    EllipticProblemDriver(para);
+    if strcmp(pde_type,'elliptic')
+        
+        para = SetParameters_Ellipitc(varargin{:});
+        
+        class_t=para.precision;
+        
+        EllipticProblemDriver(para);
+    elseif strcmp(pde_type,'maxwell')
+        
+        para = SetParameters_Maxwell(varargin{:});
+        
+        class_t=para.precision;
+        
+        MaxwellProblemDriver(para);
+        
+    else
+        error("Wrong PDE type!");
+    end
+    
 end
