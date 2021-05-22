60aad3b6bdb5f2d64348c5eccd491fc0133a1b3c
Shiqiang Xia <xiaxx268@umn.edu>
2021-05-22 16:02:36 -0500

    new marking strategy

--- 07-Visualization/PercentPlot.m
+++ 07-Visualization/PercentPlot.m
@@ -4,12 +4,15 @@ function PercentPlot(s,j)
     nn = size(s,1);
     mylist = zeros(nn,1);
     for ii = 1:nn
-        mylist(ii) = sum(s(1:ii))/total;
-        %mylist(ii) = s(ii)/total;
+        %mylist(ii) = sum(s(1:ii))/total;
+        mylist(ii) = s(ii)/total;
     end
     
     figure;
     plot(1:nn,mylist,'x--');
     xline(j);
+    
+    eq = 1/nn;
+    yline(eq,'--r');
     title('percent vs N');
 end
