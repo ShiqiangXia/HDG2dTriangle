910eb059ae9f1dea776dfd77be23355438ba9237
Shiqiang Xia <xiaxx268@umn.edu>
2021-06-02 16:59:24 -0500

    clear report format and save plots

--- 00-TestScripts/test_adaptive_refinement.m
+++ 00-TestScripts/test_adaptive_refinement.m
@@ -1,16 +1,19 @@
 
 refine_flag = 1;
-Niter_max = 4;
-%TOL_list = [1e-5,1e-6,1e-7];
+Niter_max = 6;
+%TOL_list = [1e-6,1e-7,1e-8];
 TOL_list = [1e-14];
 N_TOL = size(TOL_list,2);
+
+smooth_flag =0;
+
 flag_func1 = 0;
 flag_func2 = 0;
 flag_func3 = 1;
 
-flag_k1    = 0;
-flag_k2    = 1;
-flag_k3    = 0;
+flag_k1    = 1;
+flag_k2    = 0;
+flag_k3    = 1;
 
 if refine_flag == 0
     fprintf('Uniform Refinement')
@@ -28,17 +31,28 @@ end
     fprintf('--------------------------------------------------------------\n')
     fprintf('Initial mesh h0 = %f\n',h0);
     fprintf('\n\nTest Linear functinals J(u) = (u,g)\n\n')
-    fprintf('\n\nCase: u corner singularity and g=1 smooth\n')
+    
 
     pb_type = 2011;
     dom_ype = 'Rec';
+    
+    if smooth_flag == 0
+        pri  = 1;
+        adj  = 1;  
+        fprintf('\n\nCase: u corner singularity and g=1 smooth\n')
+    else
+        pri  = 0;
+        adj  = 0;
+        fprintf('\n\nCase: u smooth and g smooth\n')
+    end
+    
 
     for ii = 1:N_TOL
         fprintf('TOL : %.2e\n',TOL_list(ii))
         if flag_k1>0
             fprintf('--------- k = 1 --------------\n')
             main('elliptic','order',1, 'h0',h0, 'Niter',Niter_max, 'refine_flag', refine_flag,...
-            'pb_type',pb_type,'dom_type',dom_ype,'primal',1,'adjoint',1,...
+            'pb_type',pb_type,'dom_type',dom_ype,'primal',pri,'adjoint',adj,...
             'post_process_flag',1,'err_cal_flag',1,'tol_adp',TOL_list(ii));
         end
         
@@ -46,14 +60,14 @@ end
         if flag_k2>0
             fprintf('--------- k = 2 --------------\n')
             main('elliptic','order',2, 'h0',h0, 'Niter',Niter_max, 'refine_flag', refine_flag,...
-            'pb_type',pb_type,'dom_type',dom_ype,'primal',1,'adjoint',1,...
+            'pb_type',pb_type,'dom_type',dom_ype,'primal',pri,'adjoint',adj,...
             'post_process_flag',1,'err_cal_flag',1,'tol_adp',TOL_list(ii));
         end
         
         if flag_k3>0
             fprintf('--------- k = 3 --------------\n')
             main('elliptic','order',3, 'h0',h0, 'Niter',Niter_max, 'refine_flag', refine_flag,...
-            'pb_type',pb_type,'dom_type',dom_ype,'primal',1,'adjoint',1,...
+            'pb_type',pb_type,'dom_type',dom_ype,'primal',pri,'adjoint',adj,...
             'post_process_flag',1,'err_cal_flag',1,'tol_adp',TOL_list(ii));
 
         end
@@ -69,17 +83,27 @@ if flag_func2 >0
     h0 = 0.2;
     fprintf('Initial mesh h0 = %f\n',h0);
     fprintf('\n\nTest Linear functinals J(u) = <q*n, psi>\n\n')
-    fprintf('\n\nCase: u corner singularity and v discontinuous \n')
+    
     
     pb_type = 2012;
     dom_ype = 'Rec';
+    
+    if smooth_flag == 0
+        pri  = 1;
+        adj  = 2;
+        fprintf('\n\nCase: u corner singularity and v discontinuous \n')
+    else
+        pri  = 0;
+        adj  = 4;
+        fprintf('\n\nCase: u smooth and g smooth\n')
+    end
 
     for ii = 1:N_TOL
         fprintf('TOL : %.2e\n',TOL_list(ii))
         if flag_k1>0
             fprintf('--------- k = 1 --------------\n')
             main('elliptic','order',1, 'h0',h0, 'Niter',Niter_max, 'refine_flag', refine_flag,...
-            'pb_type',pb_type,'dom_type',dom_ype,'primal',1,'adjoint',2,...
+            'pb_type',pb_type,'dom_type',dom_ype,'primal',pri,'adjoint',adj,...
             'post_process_flag',1,'err_cal_flag',1,'tol_adp',TOL_list(ii));
         end
         
@@ -87,7 +111,7 @@ if flag_func2 >0
         if flag_k2>0
             fprintf('--------- k = 2 --------------\n')
             main('elliptic','order',2, 'h0',h0, 'Niter',Niter_max, 'refine_flag', refine_flag,...
-            'pb_type',pb_type,'dom_type',dom_ype,'primal',1,'adjoint',2,...
+            'pb_type',pb_type,'dom_type',dom_ype,'primal',pri,'adjoint',adj,...
             'post_process_flag',1,'err_cal_flag',1,'tol_adp',TOL_list(ii));
         end
         
@@ -95,7 +119,7 @@ if flag_func2 >0
         if flag_k3>0
             fprintf('--------- k = 3 --------------\n')
             main('elliptic','order',3, 'h0',h0, 'Niter',Niter_max, 'refine_flag', refine_flag,...
-            'pb_type',pb_type,'dom_type',dom_ype,'primal',1,'adjoint',2,...
+            'pb_type',pb_type,'dom_type',dom_ype,'primal',pri,'adjoint',adj,...
             'post_process_flag',1,'err_cal_flag',1,'tol_adp',TOL_list(ii));
 
         end
@@ -109,16 +133,27 @@ end
  if flag_func3>0
     fprintf('--------------------------------------------------------------\n')
     fprintf('--------------------------------------------------------------\n')
-
+    
+    h0 = 0.25;
     fprintf('\n\nTest Eigenvalue problem\n\n')
     fprintf('Initial mesh h0 = %f, tag_eig = 1;\n',h0);
-    fprintf('\n\nCase:  unit square\n')
-    h0 = 0.25;
+    
+    
     pb_type = 2110;
-    dom_ype = 'L';
-    Niter_max = 3;
+    
+    Niter_max = 6;
     %TOL_list = [5*1e-3,5*1e-4,5*1e-5];
-    N_TOL = size(TOL_list,2);
+    %N_TOL = size(TOL_list,2);
+    
+    if smooth_flag == 0
+        dom_ype = 'L';
+        fprintf('\n\nCase:  L shape\n')
+    else
+         dom_ype = 'Rec';
+         fprintf('\n\nCase:  unit square\n')
+    end
+    
+    
 
     for ii = 1:N_TOL
         fprintf('TOL : %.2e\n',TOL_list(ii))
