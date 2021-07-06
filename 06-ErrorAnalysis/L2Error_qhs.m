024a2656aedc5104594a98faad6f6d16900d5c8d
Shiqiang Xia <xiaxx268@umn.edu>
2021-07-06 17:27:47 -0500

    add some error functions

---
+++ 06-ErrorAnalysis/L2Error_qhs.m
@@ -0,0 +1,3 @@
+function L2Error_qhs()
+    % compute the error of qh1 - qh2
+end
---
+++ 06-ErrorAnalysis/L2Error_uhs.m
@@ -0,0 +1,4 @@
+function L2Error_uhs()
+    % compute error of  uh1 - uh2 (they may have different degrees)
+      
+end   
