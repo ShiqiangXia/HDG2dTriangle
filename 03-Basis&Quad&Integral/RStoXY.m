6d1493a73414ce7aa185e5760b16168932fdea79
Shiqiang Xia <xiaxx268@umn.edu>
2021-07-16 12:03:41 -0500

    script to compute uh at coarse GQ points

--- 03-Basis&Quad&Integral/RStoXY.m
+++ 03-Basis&Quad&Integral/RStoXY.m
@@ -1,5 +1,7 @@
 function [x,y] = RStoXY(r_list,s_list,Jk,vertice_list)
     
+    % [x,y]' = a_mat *[r+1,s+1]'+[x1,y1]'
+    
     [AffineMap,~] = Ref_Tri_Map(Jk,vertice_list);
     
     V1 = vertice_list(1,:)';
---
+++ 03-Basis&Quad&Integral/XYtoRS.m
@@ -0,0 +1,17 @@
+function [r,s] = XYtoRS(x_list,y_list,Jk,vertice_list)
+    
+    % map point(x,y) from phy element to point (r,s ) on ref element
+    % [r,s]' = a_mat^-1 *[x-x1,y-y1]'+[-1,-1]'
+    
+    
+    [~,Inv_AffineMap] = Ref_Tri_Map(Jk,vertice_list);
+    V1 = vertice_list(1,:)';
+    
+    pts_v = [x_list';y_list'] - V1;
+    
+    temp = Inv_AffineMap*pts_v + [-1,-1];
+    
+    r = temp(1,:)';
+    s = temp(2,:)';
+    
+end
