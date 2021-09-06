5bd639331792e887ea4554baa3fdd38df01b27a3
Shiqiang Xia <xiaxx268@umn.edu>
2021-09-06 15:03:45 -0500

    test AChstar for smooth case

--- 09-ConvolutionFiltering/03-GQ_helper/GetGradUhstarFromUhstarGQ.m
+++ 09-ConvolutionFiltering/03-GQ_helper/GetGradUhstarFromUhstarGQ.m
@@ -1,8 +1,24 @@
-function [grad_uH_star_x_GQ, grad_uH_star_y_GQ] ...
-                    = GetGradUhstarFromUhstarGQ(k,uH_star_GQ,GQ1DRef_pts,hx,hy)
-                
+function [grad_uH_x_GQ, grad_uH_y_GQ] ...
+                    = GetGradUhstarFromUhstarGQ(uH_DG,GQ1DRef_pts,hx,hy)
+                k = uH_DG.k;
                 Lv = Vandermonde1D(k,GQ1DRef_pts);
-                Iv = Lv\(V1D');
-                IvT = Iv';
                 Dv = GradVandermonde1D(k,GQ1DRef_pts);
+                NGQ = length(GQ1DRef_pts);
+                [~,~,Nele] = size(uH_DG.dg_coeff);
+                grad_uH_x_GQ = zeros(NGQ,NGQ,Nele);
+                grad_uH_y_GQ = zeros(NGQ,NGQ,Nele);
+                for ii = 1:uH_DG.Ny
+                    for jj = 1: uH_DG.Nx
+                        if uH_DG.mask(ii,jj) == 0
+                            temp_ele_id = (ii-1)*uH_DG.Nx+jj;
+                            temp_uh_coeff = uH_DG.dg_coeff(:,:,temp_ele_id);
+                            temp_grad_uh_x = Dv * temp_uh_coeff * Lv' * 2.0/hx;
+                            temp_grad_uh_y = Lv * temp_uh_coeff * Dv'* 2.0/hy;
+                            grad_uH_x_GQ(:,:,temp_ele_id) = temp_grad_uh_x;
+                            grad_uH_y_GQ(:,:,temp_ele_id) = temp_grad_uh_y;
+                            
+                        end
+                    end
+                end
+                
 end
